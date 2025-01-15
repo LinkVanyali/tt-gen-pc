6d959cf300c19c6002e2b66c23c88440e2372d75
LinkVanyali <45391126+LinkVanyali@users.noreply.github.com>
2025-01-15 14:22:20 +0200

    Update and rename vite-config.js to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tt-gen-pc/',  // Replace with your repository name
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
+  base: '/tt-gen-pc/'
 })
