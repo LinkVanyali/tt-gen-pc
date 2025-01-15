844630ee3c77951d768eb34ca35c6516d1a7a929
LinkVanyali <45391126+LinkVanyali@users.noreply.github.com>
2025-01-15 14:06:49 +0200

    Update vite-config.js

--- vite-config.js
+++ vite-config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/tt-gen-pc/',  // Replace with your repository name
   build: {
     outDir: 'dist',
     sourcemap: true
